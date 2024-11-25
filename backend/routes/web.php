5cf071b3f43d560b89f8eb2726ec1119e8d69c0e
xChezY <it@leonphan.dev>
2024-11-25 16:56:47 +0100

    backend

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\FCMToken;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Das sollte später entfernt werden
+Route::get('/clear', function () {
+    FCMToken::truncate();
+    return Response()->json(['message' => 'All tokens were deleted']);
 });
