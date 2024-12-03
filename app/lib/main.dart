5d6f3330b859017fb730d25cb90fd374b1f474dd
xChezY <it@leonphan.dev>
2024-12-03 18:10:13 +0100

    add firebase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:background_fetch/background_fetch.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hapy_vernetzt_app/android_webview.dart';
 import 'package:hapy_vernetzt_app/notifications.dart';
 import 'package:hapy_vernetzt_app/ios_webview.dart';
-import 'package:webview_cookie_manager/webview_cookie_manager.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
@@ -21,9 +21,9 @@ WebKitWebViewController? ioscontroller;
 
 AndroidWebViewController? androidcontroller;
 
-WebviewCookieManager cookiemanager = WebviewCookieManager();
-
 FlutterSecureStorage storage = const FlutterSecureStorage();
+
+bool isFlutterLocalNotificationsInitialized = false;
 
 String starturl = 'https://hapy-vernetzt.de/signup/';
 
@@ -31,31 +31,37 @@ int id = 0;
 
 int notificationid = -1;
 
+Future<Map<String, String>> getCookies(dynamic controller) async {
+  String cookies = await controller.runJavaScriptReturningResult('document.cookie') as String;
+  List<String> cookielist = cookies.split(';');
+  Map<String, String> cookieMap = {};
+  for (var cookie in cookielist) {
+    var cookieParts = cookie.split('=');
+    if (cookieParts.length == 2) {
+      cookieMap[cookieParts[0].trim()] = cookieParts[1].trim();
+    }
+  }
+  return cookieMap;
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  showNotification();
+  debugPrint('Handling a background message ${message.messageId}');
+}
+
 Future<void> main() async {
-  //TODO Gruppierung von Benachrichtigungen (beide Betriebssysteme)
-  //TODO Auch noch Background für Android machen
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  debugPrint(await FirebaseMessaging.instance.getToken());
+
   WidgetsFlutterBinding.ensureInitialized();
 
   initializeNotifications();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
-  await BackgroundFetch.configure(
-    BackgroundFetchConfig(
-        minimumFetchInterval: 15,
-        stopOnTerminate: false,
-        enableHeadless: true,
-        startOnBoot: true,
-        requiresBatteryNotLow: false,
-        requiresCharging: false,
-        requiresStorageNotLow: false,
-        requiresDeviceIdle: false,
-        requiredNetworkType: NetworkType.NONE),
-    (String taskId) async {
-      showNotification();
-      BackgroundFetch.finish(taskId);
-    },
-  );
-
-  repeatNotification();
 
   if (Platform.isIOS) {
     runApp(const IOSWebViewPage());
