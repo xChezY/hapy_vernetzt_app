5c2ead1dccccd7120911a3667d7f3e9dcd822159
xChezY <it@leonphan.dev>
2024-12-06 11:04:12 +0100

    add api token authentication middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FCMController;
+use App\Http\Middleware\EnsureApiTokenIsValid;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/send-device-token', [FCMController::class, 'sendDeviceToken']);
+Route::post('/send-device-token', [FCMController::class, 'sendDeviceToken'])->middleware(EnsureApiTokenIsValid::class);
