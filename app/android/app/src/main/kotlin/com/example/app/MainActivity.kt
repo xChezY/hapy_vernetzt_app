071576878202266ca3590d104eb57ef6ca4ec95d
xChezY <it@leonphan.dev>
2024-12-08 15:46:10 +0100

    remove firebase options

--- app/android/app/src/main/kotlin/com/example/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hapyVe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- app/lib/ios_notification.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
-DarwinInitializationSettings initialiseIOSNotification(){
-  return const DarwinInitializationSettings(
-    requestAlertPermission: true,
-  );
-}
