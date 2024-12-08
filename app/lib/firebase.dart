2eb14fa606769cfd028d691d9337d114b3d44e3a
xChezY <it@leonphan.dev>
2024-12-08 15:51:23 +0100

    fixed untracked files

---
+++ app/lib/firebase.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:hapy_vernetzt_app/env.dart';
+import 'package:hapy_vernetzt_app/main.dart';
+import 'package:http/http.dart' as http;
+
+void initToken() async {
+  String token = '';
+  if(token.isNotEmpty){
+    return;
+  }
+  if (Platform.isIOS){
+    token = await FirebaseMessaging.instance.getAPNSToken() ?? '';
+  }
+  if (Platform.isAndroid){
+    token = await FirebaseMessaging.instance.getToken() ?? '';
+  }
+  if(token.isNotEmpty){
+    await http.post(Uri.parse('${Env.backendurl}/api/send-device-token'), body: {
+      'Token': token,
+      'Authorization': 'Bearer ${Env.apitoken}',
+    });
+    await storage.write(key: 'token', value: token);
+  }
+}
+
+void initOnTokenRefresh(){
+  FirebaseMessaging.instance.onTokenRefresh.listen((token) async {
+
+    await http.post(Uri.parse('${Env.backendurl}/api/send-device-token'), body: {
+      'Token': token,
+      'Authorization': 'Bearer ${Env.apitoken}',
+    });
+    await storage.write(key: 'token', value: token);
+  });
+}
