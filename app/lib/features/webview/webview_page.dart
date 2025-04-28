9ce51a8d16c81acec20cb12ad7990173b4dabbf1
xChezY <it@leonphan.dev>
2025-04-28 21:44:59 +0200

    change to newer user agent

--- app/lib/features/webview/webview_page.dart
+++ app/lib/features/webview/webview_page.dart
@@ -36,7 +36,7 @@ class _WebViewPageState extends State<WebViewPage> {
                 iframeAllowFullscreen: true,
                 javaScriptCanOpenWindowsAutomatically: true,
                 useShouldOverrideUrlLoading: true,
-                userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+                userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
                 iframeAllow: "camera; microphone",
                 javaScriptEnabled: true,
                 domStorageEnabled: true,
